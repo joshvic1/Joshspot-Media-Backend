9963a9cef8502ec88aaf7fe555132c698c52febc
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2026-03-11 01:11:41 +0100

    deployed backend

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "https://joshspot-media.vercel.app",
+    "https://joshspotmedia.com",
+    "https://www.joshspotmedia.com",
+    "http://localhost:3000",
   );
   res.header(
     "Access-Control-Allow-Headers",
